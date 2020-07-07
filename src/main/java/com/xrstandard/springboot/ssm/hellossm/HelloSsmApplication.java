4a0df9f1450941d7414f14f22b5633c9b5dc1443
marsxr <xurui3333@outlook.com>
2020-07-07 19:08:01 +0800

    第一次版本提交

---
+++ src/main/java/com/xrstandard/springboot/ssm/hellossm/HelloSsmApplication.java
@@ -0,0 +1,13 @@
+package com.xrstandard.springboot.ssm.hellossm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSsmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSsmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xrstandard/springboot/ssm/hellossm/HelloSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xrstandard.springboot.ssm.hellossm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
